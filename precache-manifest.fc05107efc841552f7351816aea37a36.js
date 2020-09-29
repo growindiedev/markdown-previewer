5ee8427133a36cff7a8719f26a52dda95d057449
Abhishek Ranjan <rabhishekranjan@gmail.com>
2020-09-29 16:29:35 +0530

    Updates

---
+++ precache-manifest.fc05107efc841552f7351816aea37a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb862164d38e862eca9e2bab5143812",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "c9d43701e216d88e36b4",
+    "url": "/markdown-previewer/static/css/main.8358d289.chunk.css"
+  },
+  {
+    "revision": "f3317746daa1513a75a7",
+    "url": "/markdown-previewer/static/js/2.4467d6cf.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/markdown-previewer/static/js/2.4467d6cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9d43701e216d88e36b4",
+    "url": "/markdown-previewer/static/js/main.fcaf3b83.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jarryingnut/markdown-previewer.git/precache-manifest.219e0830480650532a8c499a8228802b.js"
+  "/markdown-previewer/precache-manifest.fc05107efc841552f7351816aea37a36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jarryingnut/markdown-previewer.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/markdown-previewer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
