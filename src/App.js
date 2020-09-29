6395714b069057215e64445ea8f2d2bfc3f1a42b
Abhishek Ranjan <rabhishekranjan@gmail.com>
2020-09-29 15:45:59 +0530

    ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Form, useFormik } from 'formik';
+import './styles.css';
+//import marked from 'marked';
+import placeholder from './placeholder';
+
+let marked = require('marked');
+//adjusts links in marked to make sure they open in new tab
+marked.setOptions({
+	breaks: true
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const formik = useFormik({
+		initialValues: {
+			editor: placeholder
+		},
+		onSubmit: (values) => {
+			console.log(values);
+		}
+	});
+
+	return (
+		<div id="main_wrapper">
+			<form onSubmit={formik.handleSubmit}>
+				<div id="edit_wrapper" className="editor">
+					<div className="headbar">
+						<span>Editor</span>
+					</div>
+					<textarea
+						id="editor"
+						class="editor"
+						type="text"
+						name="editor"
+						value={formik.values.editor}
+						onChange={formik.handleChange}
+					/>
+				</div>
+			</form>
+			<div id="preview_wrapper" className="preview">
+				<div className="headbar">
+					<span>Preview</span>
+				</div>
+				<div id="preview" dangerouslySetInnerHTML={{ __html: marked(formik.values.editor) }} />
+			</div>
+
+			{/*<div>
+				<div>
+					<div>preview</div>
+					<div id="preview" dangerouslySetInnerHTML={{ __html: marked(formik.values.editor) }} />
+				</div>
+			</div> */}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/placeholder.js
@@ -0,0 +1,40 @@
+const placeholder = `# Here you will find a wonderful React Markdown Previewer! (this is a h1 sized header!)
+
+## Below you will find several different examples of how to use this editor! (this is a h2 sized header, note the number of # used to denote what h-number is used)
+
+### Before we start, if you would like to see some of my other projects, check me out at my ) [GitHub](https://github.com/jarryingnut)!
+
+You make \`<p>inline code</p>\` with backticks!
+
+You can also make multiline code by using three backticks in the beginning and end of your code, like this:
+
+\`\`\`
+class App extends React.Componenet {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <span> This is so cool! </span>
+    )
+  }
+}
+\`\`\`
+
+> Block Quotes will look like this!
+
+- Lists are super simple as well!
+   - Indentation will change your bullet point
+1. And you can even number your list items!
+
+**bold** text works like this...
+and _italic_ like this...
+**_or even both_** like this!
+always be sure to ~~correct your mistakes~~
+
+
+### Images will even work here!
+![Tehc](https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRgRjcU7ja96B0D3S_65rP1VqkP2oFvnGdCTw&usqp=CAU)
+`;
+
+export default placeholder;
